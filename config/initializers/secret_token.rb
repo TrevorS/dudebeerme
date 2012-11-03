6002778d82d785904f932575ea4505c12e7be89c
Trevor Strieber <trevor@strieber.org>
2012-11-03 12:21:49 -0400

    Initial Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dudebeerme::Application.config.secret_token = '6b698c76a07a4371bcedd653cfd704b51f0ee251a3f6ae6028219cb32969036c40e87ac911868e345b53775f3d48737e56fff934c677e38277fe09fb7a6ee65f'
